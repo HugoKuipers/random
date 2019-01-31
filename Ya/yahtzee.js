8c6552bf2fbe9dee79da8f21dcaa4368cc28b432
Gerard Oosterhof <gerard@scripthelpt.nl>
2019-01-31 17:32:14 +0100

    Some suggestions

--- Ya/yahtzee.js
+++ Ya/yahtzee.js
@@ -1,129 +1,120 @@
-'use strict'
+"use strict";
 
-const dSides = document.getElementsByName('dice-sides')[0]
-const scoreType = document.getElementsByName('score-type')[0]
-const start = document.getElementById('start-btn')
-const dScore = document.getElementById('score-row')
-const diceDiv = document.getElementById('dice')
-const optNum = document.getElementsByClassName('opt-num')
+const dSides = document.getElementsByName("dice-sides")[0];
+const scoreType = document.getElementsByName("score-type")[0];
+const start = document.getElementById("start-btn");
+const dScore = document.getElementById("score-row");
+const tScore = document.getElementById("score-type-row");
+const diceDiv = document.getElementById("dice");
+const optNum = document.getElementsByClassName("opt-num");
 
 function play() {
-  let sides = parseInt(dSides.value)
-  let type = scoreType.value
-  if(sides < 1 || isNaN(sides)) return
+  let sides = parseInt(dSides.value);
+  let type = scoreType.value;
+  if (sides < 1 || isNaN(sides)) return;
 
-  let dice = rollDice(sides)
+  let [dice, chance] = rollDice(sides);
 
-  let multi = 1
-  if(type == 'Relative') multi = ((sides-1)/5)**2
-  let score = calcScore(dice,multi)
+  let multi = 1;
+  if (type == "Relative") multi = ((sides - 1) / 5) ** 2;
+  let score = calcScore(dice, chance, multi);
 
-  updateDisplay(dice,score)
+  updateDisplay(score);
 }
 
 function rollDice(sides) {
-  let dice = {}
-  for(let i = 0; i < 5; i++) {
-    let n = Math.ceil(Math.random()*sides)
-    if(dice[n]) {
-      dice[n]++
-    } else {
-      dice[n] = 1
-    }
+  // Could create the dice in the html alrdy here, looks a bit more random that way
+  let dHtml = "";
+  let dice = {};
+  let chance = 0;
+  for (let i = 0; i < 5; i++) {
+    let n = Math.ceil(Math.random() * sides);
+    dHtml += `<div>${n}</div>`;
+    chance += n;
+    // An alternative
+    dice[n] = dice[n] ? dice[n] + 1 : 1;
   }
-  return dice
+  diceDiv.innerHTML = dHtml;
+  return [dice, chance];
 }
 
-function calcScore(dice,multi) {
-  let score = []
-  let scoreEx = {}
-  let strait = []
-  let kindF = false
-  let fullH = [false,false]
-  let chance = 0
-  for(var i = 0; i < 13; i++) {
-    score.push(0)
+function calcScore(dice, chance, multi) {
+  let score = Object.assign({}, dice);
+  score["3 of a kind"] = 0;
+  score["4 of a kind"] = 0;
+  score["Small strait"] = 0;
+  score["Large strait"] = 0;
+  score["Full House"] = 0;
+  score["Yahtzee"] = 0;
+  score["Chance"] = chance;
+  console.log(score);
+
+  let strait = [];
+  let fullH = [false, false];
+
+  for (let die in dice) {
+    let amount = dice[die];
+    score[die] = die * amount;
+    strait.push(parseInt(die));
+
+    if (amount === 2) {
+      if (fullH[1]) {
+        score["Full House"] = 25 * multi;
+        continue;
+      }
+
+      fullH[0] = true;
+    } else if (amount >= 3) {
+      score["3 of a kind"] = chance;
+
+      if (fullH[0]) {
+        score["Full House"] = 25 * multi;
+        continue;
+      }
+
+      fullH[1] = true;
+    }
+
+    if (amount >= 4) score["4 of a kind"] = chance;
+
+    if (amount === 5) score["Yahtzee"] = 50 * multi;
   }
 
-  for(let d in dice) {
-    let a = dice[d]
-    chance += d * a
-    strait.push(parseInt(d))
+  if (strait.length < 4) return score;
 
-    if(d < 7) {
-      score[d-1] = d * a
+  strait.sort();
+  let bestS = 0;
+  let nowS = 0;
+  for (let m = 0; m < strait.length - 1; m++) {
+    if (strait[m] + 1 == strait[m + 1]) {
+      nowS++;
+      if (bestS < nowS) bestS = nowS;
     } else {
-      scoreEx[d] = d * a
+      nowS = 0;
     }
-
-    if(a == 2) {
-      fullH[0] = true
-    } else if(a == 3) {
-      fullH[1] = true
-    } else if(a == 4) {
-      kindF = true
-    } else if(a == 5) {
-      score[12] = 50 * multi
-      kindF = true
+  }
+  if (bestS > 2) {
+    score[8] = 30 * multi;
+    if (bestS > 3) {
+      score[9] = 40 * multi;
     }
   }
 
-  strait.sort()
-  let bestS = 0
-  let nowS = 0
-  for(let m = 0; m < strait.length-1; m++) {
-    if(strait[m]+1 == strait[m+1]) {
-      nowS++
-      if(bestS < nowS) bestS = nowS
-    } else {
-      nowS = 0
-    }
-  }
-  if(bestS > 2) {
-    score[8] = 30 * multi
-    if(bestS > 3) {
-      score[9] = 40 * multi
-    }
-  }
-
-  score[11] = chance
-  if(fullH[1]) {
-    if(fullH[0]) {
-      score[10] = 25 * multi
-    }
-    score[6] = chance
-  } else if(kindF) {
-    score[7] = chance
-    score[6] = chance
-  }
-
-  return [score,scoreEx]
+  return score;
 }
 
-function updateDisplay(dice, score) {
-  let dHtml = ''
-  let sHtml = ''
+function updateDisplay(score) {
+  let tHtml = "";
+  let sHtml = "";
+  console.log(score);
 
-  for(let d in dice) {
-    for(let i = 0; i < dice[d]; i++) {
-      dHtml += '<div>' + d + '</div>'
-    }
+  for (const scoreType in score) {
+    tHtml += `<td>${scoreType}</td>`;
+    sHtml += `<td>${score[scoreType]}</td>`;
   }
 
-  let countEx = 0
-  for(let i in score[0]) {
-    let s = score[0][i]
-    if(s == 0 && i < 6 && Object.keys(score[1]).length > countEx) {
-      optNum[i].innerHTML = Object.keys(score[1])[countEx] + "'s"
-      sHtml += '<td>' + score[1][Object.keys(score[1])[countEx]] + '</td>'
-      countEx++
-    } else {
-      sHtml += '<td>' + s + '</td>'
-    }
-  }
-
-  dScore.innerHTML = sHtml
-  diceDiv.innerHTML = dHtml
+  tScore.innerHTML = tHtml;
+  dScore.innerHTML = sHtml;
 }
 
-start.onclick = () => play()
+start.onclick = play;
