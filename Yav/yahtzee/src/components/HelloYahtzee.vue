70cfffcdd04042acc897dacbf53c40ee2863eff3
Hugo <Hugokuipers@hotmail.com>
2019-02-21 16:51:07 +0100

    no message

--- Yav/yahtzee/src/components/HelloYahtzee.vue
+++ Yav/yahtzee/src/components/HelloYahtzee.vue
@@ -1,114 +1,238 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <div id="game">
+      <div id="options">
+        <button id="start-btn" class="center btn" @click="play">Roll them dice! ({{ round }})</button>
+        <table id="score" class="center">
+          <thead>
+            <tr id="score-type-row">
+              <th v-for="(value,key) in score">{{ key }}</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr id="score-row">
+              <td v-for="(value,key) in score" @click="pickScore(key,value)" :class="{ locked: key in lockedScore, clickable: (!(key in lockedScore)) }">{{ value }}</td>
+              <td>{{ totalScore }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div id="dice">
+        <div v-for="(die,index) in dice" @click="pickDie(index)" :class="{ locked: saveDice[index] === true, clickable: (round < 4 && !saveDice[index]) }"> {{ die }} </div>
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
   name: "HelloYahtzee",
-  props: {
-    msg: String
+
+  data() {
+    return {
+      score: {},
+      lockedScore: {},
+      dice: [],
+      saveDice : {},
+      round: 1,
+    }
+  },
+
+  computed: {
+    totalScore: function () {
+      let sum = 0;
+      for(let p in this.lockedScore) {
+        sum += this.lockedScore[p];
+      }
+      return sum;
+    }
+  },
+
+  methods: {
+    play: function() {
+      if(this.round == 4) return;
+      let dice = this.rollDice();
+      this.calcScore(dice);
+      this.round++;
+    },
+
+    rollDice: function() {
+      let dice = {};
+      this.dice = [];
+      this.score.Chance = 0;
+      for(let i = 1; i <= 6; i++) {
+        dice[i] = dice[i] ? dice[i] : 0;
+      }
+      for(let i = 0; i < 5; i++) {
+        let n;
+        if(this.saveDice[i]) {
+          n = this.saveDice[i];
+        } else {
+          n = Math.ceil(Math.random() * 6);
+          this.saveDice[i] = 0;
+        }
+        this.dice.push(n);
+        this.score.Chance += n;
+        dice[n]++;
+      }
+      return dice;
+    },
+
+    calcScore: function(dice) {
+      this.score = Object.assign({"3 of a kind": 0,"4 of a kind": 0,"Small strait": 0,"Large strait": 0,"Full House": 0,"Yahtzee": 0,"Chance": this.score.Chance}, this.lockedScore)
+
+      let strait = [];
+      let fullH = [false, false];
+
+      for(let die in dice) {
+        let amount = dice[die];
+        if(!(die in this.lockedScore)) this.score[die] = die * amount;
+        amount !== 0 ? strait.push(parseInt(die)) : '';
+
+        if(amount === 2) {
+          if(fullH[1]) {
+            if(!("Full House" in this.lockedScore)) this.score["Full House"] = 25;
+            continue;
+          }
+          fullH[0] = true;
+        } else if(amount >= 3) {
+          if(!("3 of a kind" in this.lockedScore)) this.score["3 of a kind"] = this.score.Chance;
+          if(fullH[0]) {
+            if(!("Full House" in this.lockedScore)) this.score["Full House"] = 25;
+            continue;
+          }
+          fullH[1] = true;
+        }
+
+        if(amount >= 4 && !("4 of a kind" in this.lockedScore)) this.score["4 of a kind"] = this.score.Chance;
+
+        if(amount === 5 && !("Yahtzee" in this.lockedScore)) this.score["Yahtzee"] = 50;
+      }
+
+      if(strait.length < 4) return;
+
+      let maxStrait = 0
+      let straitCount = 0
+      for(let index = 0; index < strait.length - 1; index++) {
+        if(parseInt(strait[index]) + 1 === parseInt(strait[index + 1])) {
+          straitCount++;
+        } else {
+          straitCount = 0;
+        }
+        if(straitCount > maxStrait) maxStrait = straitCount
+      }
+
+      if(maxStrait >= 3 && !("Small strait" in this.lockedScore)) this.score["Small strait"] = 30
+      if(maxStrait >= 4 && !("Large strait" in this.lockedScore)) this.score["Large strait"] = 40
+    },
+
+    pickScore: function(key,value) {
+      if(key in this.lockedScore) return;
+      this.lockedScore[key] = value;
+      this.round = 1;
+      this.dice = [];
+      this.score = {};
+    },
+
+    pickDie: function(index) {
+      if(this.round == 4) return;
+      if(this.saveDice) {
+        this.saveDice = 0;
+      } else {
+        this.saveDice = dice[index];
+      }
+    }
   }
 };
 </script>
 
+<style>
+html {
+  background-image: url("./../assets/southpark.gif");
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+}
+</style>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+p {
+  font-size: 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+#game {
+  border: 2px solid red;
+  border-radius: 25px;
+  background-color: rgb(233, 179, 83);
+  top: 10px;
+  margin: auto;
+  height: 600px;
+  width: 1000px;
+  position: relative;
+  text-align: center;
 }
-li {
+
+#dice {
+  border-top: 2px solid red;
+  top: 50%;
+  width: 100%;
+  position: absolute;
+  text-align: center;
+}
+
+#dice div {
+  margin: 90px 40px;
+  width: 100px;
+  height: 100px;
+  position: relative;
+  border: 2px red solid;
+  border-radius: 8px;
+  text-align: center;
+  font-size: 90px;
   display: inline-block;
-  margin: 0 10px;
+  color: rgb(0, 33, 91);
 }
-a {
-  color: #42b983;
+
+#score, #score th, #score td {
+  margin: auto;
+  margin-top: 40px;
+  border: 1px solid black;
+  border-collapse: collapse;
+  padding: 6px;
+}
+
+#score {
+  width: 75%;
+}
+
+.center {
+  text-align: center;
+}
+
+.inv {
+  display: none;
+}
+
+.btn {
+  margin: auto;
+  margin-top: 50px;
+  position: relative;
+  border: 1px solid rgb(222, 93, 1);
+  font-size: 60px;
+  border-radius: 6px;
+  cursor: pointer;
+  background-color: rgb(228, 199, 153);
+}
+
+.locked {
+  background-color: rgb(208, 146, 35);
+  font-weight: bold;
+  cursor: ;
+}
+
+.clickable {
+  cursor: pointer;
 }
 </style>
